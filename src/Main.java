a02c59d43db4cb7cb68e51cc4d2d943d5c8f5abc
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2022-12-21 13:28:14 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {    // main method
+
+        int name [] = {12,32,45,56};      // one dimensional array
+        // for each loop
+        for (int a:name) {
+            System.out.print(" "+a);
+        }
+
     }
+
+
 }
